3fdd77edec53c3eeba5950a0b104d4aca8613196
Arno Debelle <debelle.a@gmail.com>
2022-02-15 14:35:13 +0100

    refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,6 @@ class TasksController < ApplicationController
   end
 
   def show
-    # @task = Task.find(params[:id])
   end
 
   def new
@@ -19,17 +18,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    # @task = Task.find(params[:id])
   end
 
   def update
-    # @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    # @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
